38b8c7b89de168777f58f4ac9b201f86c4029087
saraswatanmol202-dev <saraswatanmol202@gmail.com>
2026-03-05 17:02:34 +0530

    commited by n8n

---
+++ backend/models/user_role.py
@@ -0,0 +1,7 @@
+# Epic Title: Define User Roles
+
+class UserRole:
+    def __init__(self, user_role_id: int, user_id: int, role_id: int):
+        self.user_role_id = user_role_id
+        self.user_id = user_id
+        self.role_id = role_id
