62228853a496ab7b85a62abf91be2efa5aafc778
saraswatanmol202-dev <saraswatanmol202@gmail.com>
2026-03-05 17:03:17 +0530

    commited by n8n

---
+++ backend/models/policy.py
@@ -0,0 +1,7 @@
+# Epic Title: Access Policies for Different Roles
+
+class Policy:
+    def __init__(self, policy_id: int, name: str, description: str):
+        self.policy_id = policy_id
+        self.name = name
+        self.description = description
