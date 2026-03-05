81e1d7e1848ca5221bb159945aee9576c69f128d
saraswatanmol202-dev <saraswatanmol202@gmail.com>
2026-03-05 17:02:33 +0530

    commited by n8n

---
+++ backend/models/role.py
@@ -0,0 +1,7 @@
+# Epic Title: Define User Roles
+
+class Role:
+    def __init__(self, role_id: int, name: str, description: str):
+        self.role_id = role_id
+        self.name = name
+        self.description = description
