5417966c11b6e72b59b119ef43177a627cb6cfb3
saraswatanmol202-dev <saraswatanmol202@gmail.com>
2026-03-06 11:22:29 +0530

    commited by n8n

--- backend/views/login.py
+++ backend/views/login.py
@@ -1,15 +1,21 @@
-# Epic Title: Implement Secure Login Mechanism
+# Epic Title: Create Secure User Sessions
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from models.user import User
+from models.session import Session
 from werkzeug.security import check_password_hash
+from backend import db
 
 login_blueprint = Blueprint('login', __name__)
 
 @login_blueprint.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
+    data = request.form
     user = User.query.filter_by(username=data.get('username')).first()
     if user and check_password_hash(user.password, data.get('password')):
-        return jsonify({"message": "Password correct", "user_id": user.id}), 200
+        user_session = Session(user_id=user.id)
+        db.session.add(user_session)
+        db.session.commit()
+        session['user_id'] = user.id
+        return jsonify({"message": "Login successful", "user_id": user.id}), 200
     return jsonify({"message": "Invalid credentials"}), 401
