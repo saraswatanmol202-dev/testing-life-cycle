2e544625fa353445f8351690a8c7a67db66d7a9f
saraswatanmol202-dev <saraswatanmol202@gmail.com>
2026-03-06 11:22:30 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# Epic Title: Implement Secure Login Mechanism
+# Epic Title: Create Secure User Sessions
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-from backend.authentication.mfa import mfa_blueprint
 from backend.views.login import login_blueprint
+from backend.authentication.session import session_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/banking'
@@ -14,12 +14,13 @@ app.config['MAIL_USERNAME'] = 'username'
 app.config['MAIL_PASSWORD'] = 'password'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
+app.secret_key = 'supersecretkey'
 
 db = SQLAlchemy(app)
 mail = Mail(app)
 
-app.register_blueprint(mfa_blueprint)
 app.register_blueprint(login_blueprint)
+app.register_blueprint(session_blueprint)
 
 if __name__ == '__main__':
     db.create_all()
